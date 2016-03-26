abd05561ac31bed2c5e0dc8e6f072849ac1c708a
Kevin Law <kevin@cambly.com>
2016-03-25 19:49:30 -0700

    Add diff method to check for schedule updates.

--- game.py
+++ game.py
@@ -1,4 +1,30 @@
 class Game(object):
+    @staticmethod
+    def diff(old_schedule, new_schedule):
+        """ Compares 2 schedules and returns which schedules were added and
+            deleted in the new schedule.
+
+            Args:
+                old_schedule - A list of Games in the old schedule.
+                new_schedule - A list of Games in the new updated schedule.
+
+            Returns a tuple containing 2 lists.
+                - The first list contains the games that were in the new
+                schedule, but not in the old schedule. These should be added.
+                - The second list contains the game that were in the old
+                schedule, but removed in the new schedule. These should be
+                deleted.
+        """
+        added_games = []
+        removed_games = []
+        for game in new_schedule:
+            if game not in old_schedule:
+                added_games.append(game)
+        for game in old_schedule:
+            if game not in new_schedule:
+                removed_games.append(game)
+        return (added_games, removed_games)
+
     def __init__(self, time, opponent, is_home, location):
         self._time = time
         self._opponent = opponent
@@ -26,3 +52,8 @@ class Game(object):
                                 self._opponent,
                                 str(self._is_home),
                                 self._location)
+
+    def __eq__(self, other):
+        # Don't worry about location.
+        return (self.time == other.time and self.opponent == other.opponent and
+                self.is_home == other.is_home)
